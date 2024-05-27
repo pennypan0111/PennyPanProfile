12e19170548cf30e1ea2097889dcf7b9f52679d0
pennypan0111 <minipan0111@gmail.com>
2024-05-27 21:33:06 +0800

    add eslint rule

--- ClientApp/vite.config.js
+++ ClientApp/vite.config.js
@@ -1,12 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import eslintPlugin from 'vite-plugin-eslint'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    eslintPlugin({
+      cache: false,
+      include: ['src/**/*.vue', 'src/**/*.js', 'src/**/*.jsx', 'src/**/*.cjs', 'src/**/*.mjs']
+    })
   ],
   resolve: {
     alias: {
